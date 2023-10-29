b7ef03c1a02fd82a19804279a448de47ce3a9945
NINJATSS <143434164+NINJATSS@users.noreply.github.com>
2023-10-29 22:13:01 +0000

    Update Home.tsx

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -23,10 +23,8 @@ ENJOY AND DON'T GAMBLE WITH MORE THEN YOU CAN AFFORD TO LOSE
             target="_blank"
             icon={<Icon.ExternalLink />}
           >
-            
-          </
-        </
-      </
-    </
+        </div>
+      </Section>
+    </div>
   )
 }
