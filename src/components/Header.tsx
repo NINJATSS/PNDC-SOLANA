4ae3f88506647483d5630f4c083623a93df2d488
doctorsolana <you@example.com>
2023-11-06 20:33:08 +0100

    added logos in header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import styles from './Header.module.css'
+import React from 'react';
+import { NavButton } from './Button';
+import styles from './Header.module.css';
+import { FaDiscord, FaTwitter, FaGlobeAmericas} from 'react-icons/fa';
 
-export const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
+export const Header: React.FC<React.PropsWithChildren<any>> = ({ children }) => {
   return (
     <div className={styles.wrapper}>
       <div>
-        <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
-          <NavLink className={styles.logo} to="/">
-            <img alt="PNDC logo" src="/logo-2.svg" />
-          </NavLink>
-        </div>
-        <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
-          <div className={styles.externalLinks}>
-            <a href="https://twitter.com/Kingsol_69" target="_blank" rel="noreferrer">
-              Twitter
+        <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+          <NavButton variant="ghost" className={styles.logo} to="/">
+            <img src="/logo-2.svg" height="20px" alt="Logo" />
+            <span className={styles.title}>PNDC</span>
+          </NavButton>
+          <div className={styles.iconContainer}>
+            <a href="https://discord.gg/QZKCPpMw3g" target="_blank" rel="noreferrer" className={styles.icon}>
+              <FaDiscord />
             </a>
-            <a href="https://explorer.gamba.so" target="_blank" rel="noreferrer">
-              
+            <a href="https://twitter.com/pepesolana_pepe" target="_blank" rel="noreferrer" className={styles.icon}>
+              <FaTwitter />
+            </a>
+            <a href="https://pepesolana.com/" target="_blank" rel="noreferrer" className={styles.icon}>
+              <FaGlobeAmericas />
             </a>
           </div>
+        </div>
+        <div style={{ display: 'flex', gap: '10px' }}>
           {children}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
